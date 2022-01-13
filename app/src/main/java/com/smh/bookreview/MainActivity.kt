e5f028d3b812ecbd2b105e42f8a24f070d9631ec
MinhyeokSun <s.minyeok@gmail.com>
2022-01-13 23:31:40 +0900

    3rd Commit

--- app/src/main/java/com/smh/bookreview/MainActivity.kt
+++ app/src/main/java/com/smh/bookreview/MainActivity.kt
@@ -3,8 +3,15 @@ package com.smh.bookreview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.view.MotionEvent
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.smh.bookreview.adapter.BookAdapter
 import com.smh.bookreview.api.BookService
+import com.smh.bookreview.databinding.ActivityMainBinding
 import com.smh.bookreview.model.BestSellerDto
+import com.smh.bookreview.model.SearchBookDto
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,9 +19,17 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: BookAdapter
+    private lateinit var bookService: BookService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBookRecyclerView()
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://book.interpark.com")
@@ -22,7 +37,7 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         val bookService = retrofit.create(BookService::class.java)
-        bookService.getBestSellerBooks("99E31C8BE5814906499928BD2C9A94109473D7689F8FAE11F065A2447568DBAE")
+        bookService.getBestSellerBooks(getString(R.string.interparkAPIKey))
             .enqueue(object: Callback<BestSellerDto>{
                 override fun onResponse(
                     call: Call<BestSellerDto>,
@@ -39,7 +54,10 @@ class MainActivity : AppCompatActivity() {
                         it.books.forEach { book ->
                             Log.e(TAG, book.toString())
                         }
+                        adapter.submitList(it.books)
                     }
+
+
                 }
 
                 override fun onFailure(call: Call<BestSellerDto>, t: Throwable) {
@@ -47,6 +65,44 @@ class MainActivity : AppCompatActivity() {
                 }
 
             })
+
+        binding.searchEditText.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == MotionEvent.ACTION_DOWN) {
+                search(binding.searchEditText.text.toString())
+                return@setOnKeyListener true
+            }
+
+            return@setOnKeyListener false
+        }
+    }
+
+    private fun search(keyword: String) {
+        bookService.getBooksByName(getString(R.string.interparkAPIKey), keyword)
+            .enqueue(object: Callback<SearchBookDto>{
+                override fun onResponse(
+                    call: Call<SearchBookDto>,
+                    response: Response<SearchBookDto>
+                ) {
+                    // TODO 성공처리
+                    if (response.isSuccessful.not()) {
+                        Log.e(TAG,"not Success!!")
+                        return
+                    }
+                    adapter.submitList(response.body()?.books.orEmpty())
+                }
+
+                override fun onFailure(call: Call<SearchBookDto>, t: Throwable) {
+                    // TODO 실패처리
+                }
+
+            })
+    }
+
+    fun initBookRecyclerView() {
+        adapter = BookAdapter()
+
+        binding.bookRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.bookRecyclerView.adapter = adapter
     }
 
     companion object {
